6d967f9369841d7f51db6cea590f5c92ddbc79c9
Playermet <playermet@gmail.com>
2015-08-05 14:24:24 +0300

    Fix base example indentation;

--- examples/base.lua
+++ examples/base.lua
@@ -2,28 +2,28 @@ local glfw = require 'glfw' ('glfw3')
 
 -- Initialize the library
 if glfw.Init() == 0 then
-	return
+  return
 end
 
 -- Create a windowed mode window and its OpenGL context
 local window = glfw.CreateWindow(640, 480, "Hello World")
 if window == 0 then
-	glfw.Terminate()
-	return
+  glfw.Terminate()
+  return
 end
 
 -- Make the window's context current
-window:MakeContextCurrent()
+glfw.MakeContextCurrent(window)
 
 -- Loop until the user closes the window
-while window:ShouldClose() == 0 do
-	-- Render here
+while glfw.WindowShouldClose(window) == 0 do
+  -- Render here
 
-	-- Swap front and back buffers
-	window:SwapBuffers()
+  -- Swap front and back buffers
+  glfw.SwapBuffers(window)
 
-	-- Poll for and process events
-	glfw.PollEvents()
+  -- Poll for and process events
+  glfw.PollEvents()
 end
 
 glfw.Terminate()
